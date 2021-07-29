91f37d25bf54689ce286654a9123723e66ce6fbe
Abhishek S Mallya <mallya.abhi123@gmail.com>
2021-07-29 22:47:03 +0530

    locations

--- app/src/main/java/com/example/proton/HomeActivity.java
+++ app/src/main/java/com/example/proton/HomeActivity.java
@@ -3,20 +3,32 @@ package com.example.proton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
-/*------------------Home navigation---------------------*/
-    BottomNavigationView home_bnv;
+    Button home_loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+        home_loc = findViewById(R.id.home_location);
+
+        home_loc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent home_to_loc = new Intent(HomeActivity.this, LocationActivity.class);
+                startActivity(home_to_loc);
+            }
+        });
     }
 }
