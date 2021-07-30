ce76ccad222f1d46684f156f7360dd9d75b28eb7
Abhishek S Mallya <mallya.abhi123@gmail.com>
2021-07-30 11:56:54 +0530

    cleanup

--- app/src/main/java/com/example/proton/HomeActivity.java
+++ app/src/main/java/com/example/proton/HomeActivity.java
@@ -10,24 +10,41 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
-
-    Button home_loc;
+    FirebaseAuth mFire;
+    Button home_loc,home_signout,home_rem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-
+        home_rem = findViewById(R.id.home_reminder);
+        home_signout = findViewById(R.id.home_signout);
         home_loc = findViewById(R.id.home_location);
+
+//        home_rem.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent home_to_ala = new Intent(HomeActivity.this, AlarmActivity.class);
+//                startActivity(home_to_ala);
+//            }
+//        });
 
         home_loc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent home_to_loc = new Intent(HomeActivity.this, LocationActivity.class);
                 startActivity(home_to_loc);
+            }
+        });
+
+        home_signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this, StartActivity.class));
             }
         });
     }
